dcaf49031cc8e3391889b7f7b27a671deacf3f21
Alnader113 <147493475+Alnader113@users.noreply.github.com>
2023-12-05 10:01:48 +0100

    JScode und html Änderungen

--- JSCode.js
+++ JSCode.js
@@ -19,18 +19,6 @@ var currentUser = {
 
 };
 
-function loginUser(username, password) {
-    // Überprüfe, ob der Benutzer existiert und das Passwort korrekt ist
-    if (username === admina.username && password === admina.password) {
-        currentUser = admina; // Setze den eingeloggten Benutzer als admina
-    } else if (username === normalo.username && password === normalo.password) {
-        currentUser = normalo; // Setze den eingeloggten Benutzer als normalo
-    } else {
-        currentUser = null; // Falsche Anmeldedaten
-    }
-    return currentUser;
-}
-
 function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
@@ -53,23 +41,39 @@ function login() {
 
 }
 
-let hermannplatz = {
-    name: "Hermannplatz",
-    desc: "viel Autoverkehr, ",
-    street: "hermannplatz",
-    zip: "10967",
-    city: "Berlin",
-    state: "Berlin",
-    lat: 52.485664724,
-    lon: 13.421498314
-}
+function showUpdateDelete(location, locationData1) {
+    let hermannplatz = {
+        name: "Hermannplatz",
+        desc: "viel Autoverkehr, ",
+        street: "hermannplatz",
+        zip: "10967",
+        city: "Berlin",
+        state: "Berlin",
+        lat: 52.485664724,
+        lon: 13.421498314
+    }
 
+    let alexanderplatz = {
+        name: "Alxanderplatz",
+        desc: "Luftverschmutzung ",
+        street: "alexanderplatz",
+        zip: "10178",
+        city: "Berlin",
+        state: "Berlin",
+        lat: 52.5219814,
+        lon: 13.413635717448294
+    }
 
-
-
-
-
-function showUpdateDelete(location) {
+    let zoologischergarten = {
+        name: "S+U Bahnhof Zoologischer garten",
+        desc: "Umweltverschmutzung ",
+        street: "Hardenbergplatz 13",
+        zip: "10623",
+        city: "Berlin",
+        state: "Berlin",
+        lat: 52.5071378,
+        lon: 13.331868
+    }
     document.getElementById('mainAdmina').style.display = 'none'; // Verstecke den aktuellen Hauptbildschirm
     document.getElementById('mainNormal').style.display = 'none'; // Verstecke den aktuellen Hauptbildschirm
     document.getElementById('updateDelete').style.display = 'block'; // Zeige die updateDelete-Seite an
@@ -81,15 +85,21 @@ function showUpdateDelete(location) {
     }
     const lng = '13.421498314'
     const lat = '52.485664724'
-    document.getElementById('map').src = 'https://maps.google.com/maps?q=' + lat + ',' + lng + '&t=&z=15&ie=UTF8&iwloc=&output=embed'
+   // document.getElementById('map').src = 'https://maps.google.com/maps?q=' + lat + ',' + lng + '&t=&z=15&ie=UTF8&iwloc=&output=embed'
 
-    let locationDate = null;
+    let locationData = null;
     switch (location) {
         case 'hermannplatz':
             locationData = hermannplatz;
             break;
-        // Füge weitere Cases für andere Locations hinzu, wenn nötig
+        case 'alexanderplatz':
+            locationData = alexanderplatz;
+            break;
+        case 'zoologischergarten' :
+            locationData = zoologischergarten;
+            break;
         default:
+            locationData = locationData1;
             break;
     }
     if (locationData !== null) {
@@ -109,6 +119,22 @@ function showUpdateDelete(location) {
 function showAdminMain() {
     document.getElementById('login').style.display = 'none';
     document.getElementById('mainAdmina').style.display = 'block';
+    const locations = JSON.parse(localStorage.getItem('locations'))
+    const locationLinks = document.getElementById('locationLinks');
+    if (locationLinks && locations && locations.length > 0){
+        locations.forEach(location => {
+            const liElement = document.createElement('li');
+            const aElement = document.createElement('a');
+            aElement.innerHTML = location.city + ' - '+ location.street ;
+            aElement.href = '#'
+            aElement.onclick = ev => {
+                showUpdateDelete(location.city, location)
+            }
+            liElement.appendChild(aElement);
+            locationLinks.appendChild(liElement);
+        })
+
+    }
 }
 
 function showNormalMain() {
@@ -122,12 +148,33 @@ function openAddLocation() {
 }
 
 function logout() {
+    localStorage.removeItem('isAdmin');
     document.getElementById('login').style.display = 'block';
     document.getElementById('mainAdmina').style.display = 'none';
     document.getElementById('mainNormal').style.display = 'none';
     document.getElementById('username').value = '';
     document.getElementById('password').value = '';
 
+}
+function deleteLocation(){
+    const isAdmin = JSON.parse(localStorage.getItem('isAdmin'))
+    if(!isAdmin){
+        document.getElementById('mainNormal').style.display = 'block';
+    }else {
+        document.getElementById('mainAdmina').style.display = 'block';
+    }
+    document.getElementById('updateDelete').style.display = 'none';
+
+}
+function updateLocation(){
+    const isAdmin = JSON.parse(localStorage.getItem('isAdmin'))
+    if(!isAdmin){
+        document.getElementById('mainNormal').style.display = 'block';
+    }else {
+        document.getElementById('mainAdmina').style.display = 'block';
+    }
+    document.getElementById('updateDelete').style.display = 'none';
+    document.getElementById('login').style.display = 'none';
 }
 function cancelUpdateDelete() {
     const isAdmin = JSON.parse(localStorage.getItem('isAdmin'))
@@ -138,30 +185,67 @@ function cancelUpdateDelete() {
     }
     document.getElementById('updateDelete').style.display = 'none';
 }
+function cancelAdd() {
+
+    document.getElementById('mainAdmina').style.display = 'block';
+    document.getElementById('add').style.display = 'none';
+}
 
 
-function getCoordinatesFromAddress(address) {
+async function getCoordinatesFromAddress(address) {
     const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.length > 0) {
-                const latitude = data[0].lat;
-                const longitude = data[0].lon;
+    return new Promise((resolve, reject)=> {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data && data.length > 0) {
+                    const latitude = data[0].lat;
+                    const longitude = data[0].lon;
+                    console.log({ latitude,longitude})
+                    resolve({ latitude, longitude})
+                    document.getElementById('locationLat').value = latitude;
+                    document.getElementById('locationLon').value = longitude;
+                } else {
+                    reject(new Error('Keine Koordinaten für diese Adresse gefunden. Bitte Adresse überprüfen.'))
+                    alert('Keine Koordinaten für diese Adresse gefunden. Bitte Adresse überprüfen.');
+                }
+            })
+            .catch(error => {
+                // Fehler beim API-Aufruf
+                console.error('Fehler beim Abrufen der Koordinaten:', error);
+                reject(new Error('Fehler beim Abrufen der Koordinaten:'))
 
 
-                document.getElementById('updateLocationLat').value = latitude;
-                document.getElementById('updateLocationLon').value = longitude;
-            } else {
+                // alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.');
+            });
+    })
+}
+async function validateAddForm() {
+    const city = document.getElementById('locationCity').value;
+    const street = document.getElementById('locationStreet').value;
+    // alle werte noch
+    const description = document.getElementById('locationDescription').value;
+    const zip = document.getElementById('locationZip').value;
+    const name = document.getElementById('locationName').value
 
-                alert('Keine Koordinaten für diese Adresse gefunden. Bitte Adresse überprüfen.');
-            }
-        })
-        .catch(error => {
-            // Fehler beim API-Aufruf
-            console.error('Fehler beim Abrufen der Koordinaten:', error);
-            alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.');
-        });
+    const {latitude, longitude} = await getCoordinatesFromAddress(city + '+' + street);
+    const newLocation = {
+        zip,
+        description,
+        name,
+        city,
+        street,
+        lat: latitude,
+        lon: longitude,
+    }
+    const locations = JSON.parse(localStorage.getItem('locations') || '[]')
+    locations.push(newLocation)
+    localStorage.setItem('locations', JSON.stringify(locations))
+    document.getElementById('mainAdmina').style.display = 'block';
+    document.getElementById('add').style.display = 'none';
+
+    showAdminMain();
 }
 
